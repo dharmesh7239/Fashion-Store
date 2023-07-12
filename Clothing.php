9dfc37b8fef5bae703ae0b0e996a9a61ab213c53
Dharmesh malav <63228729+dharmesh7239@users.noreply.github.com>
2023-07-12 23:43:00 +0530

    Add files via upload

---
+++ Clothing.php
@@ -0,0 +1,63 @@
+<?php
+ include'header.php';
+?>
+
+<div class="container">
+    <h5>CLOTHING</h5>
+<div class="row">
+            <div class="col-md-3 col-sm-6 col-6">
+              <div class="product-img">
+                <img src="img/g3.png" width="100%" class="img-fluid">
+              </div>
+              <div class="product-button">
+                <button>Add to cart</button>
+              </div>
+              <div class="details text-center">
+                <h6>MetroNaut </h6> 
+                <span> Rs. 799</span>               
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-6">
+              <div class="product-img">
+                <img src="img/g1.png" width="100%" class="img-fluid">
+              </div>
+              <div class="product-button">
+                <button>Add to cart</button>
+              </div>
+              <div class="details text-center">
+                <h6>US Polo</h6> 
+                <span>Rs.  599</span>               
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6  col-6">
+              <div class="product-img">
+                <img src="img/g2.png" width="100%" class="img-fluid">
+              </div>
+              <div class="product-button">
+                <button>Add to cart</button>
+              </div>
+              <div class="details text-center">
+                <h6>Levis </h6> 
+                <span>Rs. 899</span>               
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-6">
+              <div class="product-img">
+                <img src="img/g3.png" width="100%" class="img-fluid">
+              </div>
+              <div class="product-button">
+                <button>Add to cart</button>
+              </div>
+              <div class="details text-center">
+                <h6>Metronaut </h6> 
+                <span>Rs. 599</span>               
+              </div>
+            </div>
+            
+          </div>
+</div>
+
+
+<?php
+ include'footer.php';
+?>
